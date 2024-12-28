89aff90c35aee21b19c2047e55762e054ff579b9
Bruh <177808919+Bruxki@users.noreply.github.com>
2024-12-28 17:47:22 +0600

    gave up

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -5,7 +5,10 @@ namespace WebApplication2.Data
 {
     public class ApplicationDbContext : DbContext
     {
-        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) 
+            : base(options) 
+        { 
+        }
 
         public DbSet<Category> Categories { get; set; }
         public DbSet<Product> Products {get; set; }
