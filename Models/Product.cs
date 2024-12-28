7471b1082f6b81b1404bf09efd30797eb1be6cc6
Bruh <177808919+Bruxki@users.noreply.github.com>
2024-12-28 13:53:21 +0600

    1st commit

---
+++ Models/Product.cs
@@ -0,0 +1,12 @@
+﻿namespace WebApplication2.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public double Price { get; set; }
+        //one to many?
+        public int CategoryId { get; set; }
+        public Category Category { get; set; }
+    }
+}
